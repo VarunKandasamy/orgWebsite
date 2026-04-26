6ed6f44bfd43fef3d944e6de02596bbfce38a299
Varun Kandasamy <varuntheinventor@gmail.com>
2026-04-26 18:36:38 -0500

    fixed ui bugs, updated images+bios

--- data/about.py
+++ data/about.py
@@ -1,15 +1,33 @@
 TEAM_MEMBERS = [
     {
-        "name": "Lauren Elle Darby",
+        "name": "Lauren Elle Kasule-Darby",
         "title": "Director of Research",
         "department": "Research Department",
-        "bio": "Lauren leads the Research Department, overseeing a team of seven Associate Directors. She synthesizes findings into cohesive reports and recommendations for government officials and organizational partners.",
+        "photo": "Lauren.JPEG",
+        "bio": "Lauren Elle Kasule-Darby is a rising junior at Seton Hall University, majoring in Political Science with a Double Minor in History and International Relations. However, she is originally from Marietta, Georgia. She serves as the Director of Research for the Demopolis Coalition, and her background in politics is vast, having worked under several congressional and local campaigns. She began her work in Demopolis after forming a strong connection to local politics, finding that many people in the town where she was raised knew little to nothing about the systems that govern them. Through this, she is passionate about finding solutions to issues affecting people of all backgrounds and representing those who are often unheard. She believes that understanding local politics is the key to understanding our government, which is why she's proud to work for the Demopolis Coalition. As Director, she leads a team of seven associate directors and numerous research associates, all of whom share the same goal of educating the public and finding solutions to the challenges of small-town local politics.",
+        "linkedin": None,
+        "phone": None,
+        "email": None,
     },
     {
         "name": "Ben Kelley",
         "title": "Director of Chapters",
         "department": "Chapters Program",
+        "photo": None,
         "bio": "Ben leads the Chapters Program, working to establish and support Demopolis Coalition chapters at colleges and universities across our operating states.",
+        "linkedin": None,
+        "phone": None,
+        "email": None,
+    },
+    {
+        "name": "Charlotte Smallwood",
+        "title": "Associate Director of Civic Research and Local Politics",
+        "department": "Research Department",
+        "photo": "Charlotte.JPG",
+        "bio": "Charlotte Smallwood is the Associate Director of Civic Research and Local Politics, and is from Maine. She's currently studying Political Science at the University of Maine and is proud to be a first-generation college student. She never originally saw herself getting involved in politics, but she became interested after learning about NAGPRA laws and how they impacted her local Native community, the Penobscot Tribe. Since then, she's become passionate about understanding how government decisions affect real communities and making information more accessible so others can better recognize the importance of civic engagement. This is what drew her to working for the Demopolis Coalition, as it allows her to share the information she finds with the people of small town communities.",
+        "linkedin": "https://www.linkedin.com/in/charlotte-smallwood-b5967733a/",
+        "phone": "(207) 889-2888",
+        "email": "char.smallwood777@gmail.com",
     },
 ]
 
